4f84010183523eb9d848e2c082dac4004d20c3c8
Hittry <mr.sasha.shuv@gmail.com>
2020-10-13 14:56:28 +0300

    Lab 2

--- new.js
+++ new.js
@@ -1,77 +1,171 @@
 const mUrl = '/api/ShuvalovSasha/lab1/labFirst_var2';
 var express = require('express');
 var app = express();
+var fs = require('fs');
+app.set('view engine', 'pug');
+
+fs.open('errorLog.txt', 'w+', (err, fd) => {
+	if (err) throw err;
+});
+
+
+app.use(function (req,res,next){
+    console.log(req.hostname + req.path)
+    next()
+});
+
+app.use(mUrl + "task2",function (req,res,next){
+    if(req.query.admin === 'true'){
+        next();
+    }
+    else{
+		fs.appendFile('errorLog.txt', req.hostname + req.path + " error 403 in task2" + "\n", (err) => {
+			if(err) throw err;
+			console.log('Error has been added!');
+		});
+		res.status(403).send("Not auth");
+        
+    }
+
+});
+
+app.use(mUrl + "task3",function (req,res,next){
+    console.log("task3 check:");
+    next();
+
+});
+
+app.use(mUrl + "task3",function (req,res,next){
+    if(req.query.admin === 'true'){
+        next();
+    }
+    else{
+		fs.appendFile('errorLog.txt', req.hostname + req.path + " error 403 in task3" + "\n", (err) => {
+			if(err) throw err;
+			console.log('Error has been added!');
+		});
+		res.status(403).send("Not auth");
+    }
+
+
+});
+
+
 
 app.get('/', function(request, response) {
-  response.send("<h4> "+ mUrl + "task1?fruit=Apple&fruit=Orange&fruit=bannana</h4>  "+
-  "<h4> "+ mUrl + "task2?Month1=3&Second1=40&Hours1=13&Minute1=15&Month2=5&Second2=50&Hours2=15&Minute2=41</h4>  "+
-  "<h4> "+ mUrl + "task3?Text1=Promis1_complete&Text2=Promise2_complete&Text3=error</h4>")
+  response.send("<h1>Welcome, add this string in url</h1>"+"<h4>/api/ShuvalovSasha/lab1/labFirst_var2/views/menu</h4>")
 });
 
 app.get(mUrl + "task1", function (req, res) {
+	
 	console.log(req.query)
 	let fruits = req.query.fruit;
-	let responseText = '<ul>';
-	for(let i = 0; i<2;i++){
+	let number = req.query.number;
+	let responseText = "Your array:" 
+	for(let i = 0; i<number;i++){
     		fruits.push(fruits[i]);
 	}
+	
+	for(let i = 0; i<number;i++){
+    		fruits.shift(fruits[i])
+	}
 	for (let j = 0; j < fruits.length; j++) {
-    responseText += '<li>' + fruits[j] + '</li>';
+    responseText +=fruits[j]+ " ";
   }
-	responseText += '</ul>';
- 	res.send(responseText);
+
+	res.render("/api/ShuvalovSasha/lab1/labFirst_var2/views/T1", {
+		title: "work",
+		message: responseText
+	});
+
+
 });
 
-
-
-app.get(mUrl + 'task2', function (req, res) {
+app.use("/api/ShuvalovSasha/lab1/labFirst_var2/views/menu", function (req,res){
+	res.render("/api/ShuvalovSasha/lab1/labFirst_var2/views/menu", {
+		title: "work"
+	});
+});
+app.use("/api/ShuvalovSasha/lab1/labFirst_var2/views/exampl1", function (req,res){
+	res.render("/api/ShuvalovSasha/lab1/labFirst_var2/views/exampl1", {
+		title: "work"
+	});
+});
+app.use("/api/ShuvalovSasha/lab1/labFirst_var2/views/exampl2", function (req,res){
+	res.render("/api/ShuvalovSasha/lab1/labFirst_var2/views/exampl2", {
+		title: "work"
+	});
+});
+app.use("/api/ShuvalovSasha/lab1/labFirst_var2/views/exampl3", function (req,res){
+	res.render("/api/ShuvalovSasha/lab1/labFirst_var2/views/exampl3", {
+		title: "work"
+	});
+});
+app.get(mUrl + 'task2', function (req, res) {  //формат ввода д1 = 123221 д2 =654566
 function showMinutes() {
-    let m1 = req.query.Month1;
-    let m2 = req.query.Month2;
-    let S1 = req.query.Second1;
-    let S2 = req.query.Second2;
-    let H1 =req.query.Hours1;
-    let H2 = req.query.Hours2;
-    let Min1 = req.query.Minute1;
-    let Min2 = req.query.Minute2;
+    let m1 = req.query.data1;
+    let S1 = req.query.data2;
+    let newm1 = m1.split(",");
+    let newS1 = S1.split(",");
     console.log(req.query)
     let responseText = "Month difference: ";
-    responseText += m2-m1;
+    responseText += Math.abs(newm1[0]-newS1[0]);
      responseText += " Hours difference: ";
-      responseText += H2-H1;
+      responseText += Math.abs(newm1[1]-newS1[1]);
       responseText += " Minutes difference: ";
-      responseText += Min2-Min1;
+      responseText += Math.abs(newm1[2]-newS1[2]);
       responseText += " Second difference: ";
-      responseText += S2-S1;
-    res.status(200).send((responseText).toString());
+      responseText += Math.abs(newm1[3]-newS1[3]);
+      res.render("/api/ShuvalovSasha/lab1/labFirst_var2/views/T2", {
+		title: "work",
+		message: responseText
+	});
+
   
 }
 showMinutes();
 });
-
-
 app.get(mUrl + 'task3', function (req, res) {
-const promise1 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('Promise1 выполнен');
-    }, 2000);
-});
-const promise2 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('Promise2 выполнен');
-    }, 1500);
-});
-let resText = req.query.Text1;
-let resT = req.query.Text2;
-resText += " ";
-resText += resT;
-let resTe = req.query.Text3;
-Promise.all([promise1, promise2])
-    .then((data) => res.send(resText))
-    .catch((error) => res.send(resTe));
+    let resText = req.query.Text1;
+    let resT = req.query.Text2;
+    let resTe = req.query.Text3;
+    let rek = req.query.Text4;
+    console.log(req.query)
+    const promise1 = new Promise((resolve, reject) => {
+	 setTimeout(() => resolve("Promise1"), resText);
+	 setTimeout(() => reject(new Error("ignored1")), resT);
+
+    });
+    const promise2 = new Promise((resolve, reject) => {
+	 setTimeout(() => resolve('Promise2 выполнен'),resTe);
+	 setTimeout(() => reject(new Error("ignored2")), rek);
+    });
+    Promise.all([promise1, promise2])
+	 .then((data) => res.render("/api/ShuvalovSasha/lab1/labFirst_var2/views/T3", {
+		title: "work",
+		message: "complete promise"
+	})
+	)
+	 .catch((error) => res.render("/api/ShuvalovSasha/lab1/labFirst_var2/views/T3", {
+		title: "work",
+		message: "Error"
+	}))
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.use(function (req, res, next) {
+	res.status(404).send("Not Found")
+	fs.appendFile('errorLog.txt', req.hostname + req.path + " error 404 " + "\n", (err) => {
+		if(err) throw err;
+		console.log('Error has been added!');
+	});
+	next()
 });
 
+app.listen(4001, (error) => {
+  if (error) {
+	  fs.appendFile('errorLog.txt', req.hostname + req.path + " error 500 in server" + "\n", (err) => {
+		  if(err) throw err;
+		  console.log('Error has been added!');
+	  });
+  }
+  console.error('Example port 4001')});
